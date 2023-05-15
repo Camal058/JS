6f905dd1e4779bd3f1ac5f267a9328340aaab25f
Camal <87723982+Camal058@users.noreply.github.com>
2023-05-16 00:44:17 +0400

    first

---
+++ JS1/script.js
@@ -0,0 +1,60 @@
+/* №1
+let name = prompt("What is your name?");
+alert(`Hello ${name}`);
+*/
+
+/* №2
+const thisYear = 2023;
+let birthmark = prompt("Enter year of your birth");
+birthmark = thisYear - birthmark;
+alert("You are " + birthmark + " years old");
+ */
+
+/* №3
+let length = prompt("Enter length of square");
+alert("Perimeter of square: " + 4 * length);
+ */
+
+/* №4
+let radius = prompt("Enter radius");
+alert("Square of circle: " + radius * radius * 3.14);
+ */
+
+/* №5
+let distance = prompt("Enter distance length");
+let time = prompt("How many hours?")
+alert("Your speed must be: " + distance / time);
+ */
+
+/* №6
+const euro = 0.91;
+let dollar = prompt("Enter amount of dollar");
+alert(dollar + "$ is " + euro * dollar + " euro");
+ */
+
+/* №7
+let value = prompt("Enter quantity of gb");
+alert("You can have " + (value * 1000) / 820 + " files");
+ */
+
+/* №8
+let money = prompt("Enter the amount of money in wallet");
+let ChocolatePrice = prompt("Enter price of one chocolate");
+alert("You can buy " + money / ChocolatePrice + " chocolates, and your change will be " + money % ChocolatePrice);
+ */
+
+/* №9
+let number = prompt("Enter number");
+let firstNum = number % 1000;
+thirdNum = parseInt(thirdNum / 100);
+let secNum = number % 100;
+secNum = parseInt(secNum / 10);
+let thirdNum = number % 10;
+let totalNum = `${thirdNum}${secNum}${firstNum}`;
+alert(totalNum);
+ */
+
+/* №10
+let num = prompt("Enter number");
+alert(num % 2 == 0 ? "Number is even" : "Number is odd");
+ */
